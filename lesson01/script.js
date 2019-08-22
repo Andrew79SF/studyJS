3e5ddd4cbf6dcbc15acccbc93ec2f6ad389c414d
Andrey Romanov <andrew79sf@gmail.com>
2019-08-22 04:25:45 -0700

    Lesson 01

---
+++ lesson01/script.js
@@ -0,0 +1,10 @@
+let money = "RUB";
+let income = 1000;
+let addExpenses = 200;
+let deposit = 300000;
+let mission = "JavaScript";
+let period = "6 weeks";
+
+alert("I learn JavaScript!");
+
+console.log("Hello World!");
