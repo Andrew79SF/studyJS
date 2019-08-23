396c3846ccc4349bd3a7f0729cf9141639daee08
Andrey Romanov <andrew79sf@gmail.com>
2019-08-23 13:19:51 -0700

    исправлены замечания по объявлению переменных

--- lesson02/difficult.js
+++ lesson02/difficult.js
@@ -1,10 +1,8 @@
-let num = 266219;
-
-let arr = num.toString().split('');
-
-let mult = arr.reduce(function (previous, current) {
+let num = 266219,
+		arr = num.toString().split(''),
+		mult = arr.reduce(function (previous, current) {
 			return previous * current;
-});
+		});
 
 console.log(mult);
 
