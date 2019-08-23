c4153ad42d33dcd1eb48bba0b395b67678fe059f
Andrey Romanov <andrew79sf@gmail.com>
2019-08-23 02:55:08 -0700

    Lesson 02

---
+++ lesson02/difficult.js
@@ -0,0 +1,19 @@
+let num = 266219;
+
+let arr = (num + '').split('');
+let mult = 1;
+
+for (let i = 0; i < arr.length; i++) {
+	mult *= arr[i];
+}
+
+console.log(mult);
+
+// Exponentiation
+let exp = mult;
+
+for (let pow = 3; pow > 1; pow--) {
+		exp *= mult;
+}
+
+alert((exp + '').substr(0, 2));
---
+++ lesson02/script.js
@@ -0,0 +1,21 @@
+let money = 50000;
+let income = 'freelance';
+let addExpenses = '500, 1000, 150';
+let deposit = true;
+let mission = 1000000;
+let period = 5;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+console.log(money % 30);
