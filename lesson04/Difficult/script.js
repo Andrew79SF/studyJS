e129bd371655c3a3e7d237517d2cd972c9de8bcb
Andrey Romanov <andrew79sf@gmail.com>
2019-08-27 02:21:35 -0700

    Добавил усложненное задание

---
+++ lesson04/Difficult/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Solution
+
+let test = function(arg) {
+	if (typeof arg !== 'string') {
+		return (arg + ' - Это не строка!!! Это - ' + typeof arg);
+	}
+
+	let newString = arg.trim();
+	if (newString.length > 30) {
+		newString = newString.slice(0, 30) + '...';
+	}
+	return newString;
+};
+
+
+// Test
+
+console.log(test(true));
+console.log(test(12345));
+console.log(test(null));
+console.log(test(undefined));
+console.log(test('   Hello world!!!   '));
+console.log(test('    123456789012345678901234567890123   '));
