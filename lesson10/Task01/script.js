62bcf57642081a89c40ec8f8c2cde52dbd638707
Andrey Romanov <andrew79sf@gmail.com>
2019-09-09 07:21:32 -0700

    lesson10

--- lesson10/Task01/script.js
+++ lesson10/Task01/script.js
@@ -1,39 +1,33 @@
 'use strict';
 
-function DomElement (selector, height, width, bg, fontSize) {
-	this.selector = selector;
-	this.height = height;
-	this.width = width;
-	this.bg = bg;
-	this.fontSize = fontSize;
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
 }
 
-DomElement.prototype.createElement = function() {
-	if (this.selector[0] == '.') {
-		let div = document.createElement('div');
-		div.className = this.selector.slice(1);
-		div.innerHTML = 'Create div with class = ' + this.selector.slice(1);
-		document.body.append(div);
-		div.style.height = this.height;
-		div.style.width = this.width;
-		div.style.backgroundColor = this.bg;
-		div.style.fontSize = this.fontSize;
-
-	} else if (this.selector[0] == '#') {
-		let p = document.createElement('p');
-		p.innerHTML = this.selector.slice(1);
-		document.body.append(p);
-		p.style.height = this.height;
-		p.style.width = this.width;
-		p.style.backgroundColor = this.bg;
-		p.style.fontSize = this.fontSize;
-	}
+DomElement.prototype.createElement = function () {
+  let elem;
+  if (this.selector[0] == '.') {
+    elem = document.createElement('div');
+    elem.className = this.selector.slice(1);
+  } else if (this.selector[0] == '#') {
+    elem = document.createElement('p');
+  }
+  elem.innerHTML = this.selector.slice(1);
+  document.body.append(elem);
+  elem.style.height = this.height;
+  elem.style.width = this.width;
+  elem.style.backgroundColor = this.bg;
+  elem.style.fontSize = this.fontSize;
 };
 
-let domElement = new DomElement('.hello', '300px', '500px', 'red', '24px');
+let domElement = new DomElement('.hello', '300px', '500px', 'red', '40px');
 
 domElement.createElement();
 
-let domElement2 = new DomElement('#Hello World!!!','200px', '400px', 'green', '32px');
+let domElement2 = new DomElement('#Hello World!!!', '200px', '400px', 'green', '32px');
 
 domElement2.createElement();
