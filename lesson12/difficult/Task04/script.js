31e89f35ee79fca336c01bd3b91b75e9d7972edb
Andrey Romanov <andrew79sf@gmail.com>
2019-09-14 19:26:46 -0700

    add difficult task2

---
+++ lesson12/difficult/Task04/script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+let fish = document.querySelector('.fish'),
+  elem = document.querySelector('#elem'),
+  startBtn = document.querySelector('#start'),
+  resetBtn = document.querySelector('#reset'),
+	backBtn = document.querySelector('#back'),
+  statusStart = false,
+	countX = 0,
+	countY = 25,
+	down = true,
+  idAnimate,
+	reverse = false,
+
+	swimAnimate = function () {
+		countX = getCountX(countX);
+		countY = getCountY(countY);
+		fish.style.left = countX + 'vw';
+		fish.style.top = countY + 'vh';
+		idAnimate = requestAnimationFrame(swimAnimate);
+	};
+
+startBtn.addEventListener('click', start);
+resetBtn.addEventListener('click', reset);
+backBtn.addEventListener('click', back);
+
+function start() {
+  if (!statusStart) {
+    statusStart = true;
+		idAnimate = requestAnimationFrame(swimAnimate);
+  } else {
+    statusStart = false;
+    cancelAnimationFrame(idAnimate);
+  }
+}
+
+function reset() {
+	if (reverse) {
+		back();
+	}
+	countX = 0;
+	countY = 25;
+  if (statusStart) {
+    cancelAnimationFrame(idAnimate);
+    statusStart = false;
+  }
+	fish.style.left = countX + 'vw';
+	fish.style.top = countY + 'vh';
+}
+
+function back() {
+  if (!reverse) {
+    elem.style.transform = 'scaleX(-1)';
+    reverse = true;
+  } else {
+    elem.style.transform = '';
+		reverse = false;
+	}
+}
+
+function getCountX(countX) {
+	countX = (reverse) ? countX - 0.14 : countX + 0.14;
+	if (countX < 0 || countX > 90) {
+		back();
+	}
+	return countX;
+}
+
+function getCountY(countY) {
+	if (countY < 35 && down) {
+	  countY += 0.1;
+	} else if (countY > 25) {
+	  down = false;
+	  countY -= 0.1;
+	} else {
+	  down = true;
+	}
+	return countY;
+}
